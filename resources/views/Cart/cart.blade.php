04cb01f5331f80e6b9a40c5d7cdf28d12511c9b6
Akmal Alifansyah <crabatemypizza@gmail.com>
2026-05-12 07:03:09 +0700

    Revert "fix cart checkout bug"
    This reverts commit ad1aa110370272d1f50579d5e4c455aaa9414ed7.

--- resources/views/Cart/cart.blade.php
+++ resources/views/Cart/cart.blade.php
@@ -71,10 +71,7 @@
                     <p class="text-light fw-bold fs-5 mb-3">
                         Total: {{ "Rp " . number_format($totalPrice, 0, ',', '.') }}
                     </p>
-                    <div class="gap-3 d-flex flex-row">
-                        <button id="checkoutBtnQRIS" data-bs-toggle="modal" data-bs-target="#deleteModal" class="btn btn-primary text-light px-4">Checkout with QRIS <i class="bi bi-qr-code"></i></button>
-                        <button id="checkoutBtn" data-bs-toggle="modal" data-bs-target="#deleteModal" class="btn btn-primary text-light px-4">Checkout</button>
-                    </div>
+                    <button id="checkoutBtn" data-bs-toggle="modal" data-bs-target="#deleteModal" class="btn btn-primary text-light px-4">Checkout</button>
                 </div>
             </div>
         @endif
