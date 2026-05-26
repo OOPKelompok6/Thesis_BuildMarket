65460d9c2d03985bce96be85037957d3b2975b40
Akmal Alifansyah <crabatemypizza@gmail.com>
2026-05-26 17:14:04 +0700

    update tiny mce, tighten unexpected bug control

--- public/js/tinymce/tinymceConfig.js
+++ public/js/tinymce/tinymceConfig.js
@@ -1,6 +1,14 @@
 document.addEventListener("DOMContentLoaded", function() {
     tinymce.init({ 
         selector: '#descriptionTextArea',
+        plugins: 'lists',
+        toolbar:
+            'undo redo | ' +
+            'styles | ' +
+            'bold italic | ' +
+            'alignleft aligncenter alignright | ' +
+            'bullist numlist | ' +
+            'outdent indent',
         menubar: "edit insert format",
         license_key: 'gpl', 
         removed_menuitems: 'superscript subscript code',
