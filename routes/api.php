e0471d44aa5a196c193676eb979ca288e3a2f3f6
Akmal Alifansyah <crabatemypizza@gmail.com>
2026-07-05 08:31:45 +0700

    fix shipment controller abstraction

--- routes/api.php
+++ routes/api.php
@@ -1,14 +1,14 @@
 <?php
 
 use App\Http\Controllers\Api\PaymentWebHookController;
-use App\Services\ShipmentService;
+use App\Http\Controllers\Api\ShipmentController;
 use Illuminate\Support\Facades\Route;
 
 // Payment webhook handler
 Route::post('/payments/webHookHandler', [PaymentWebHookController::class, 'handle']);
 
 //Shipment Utilities
-Route::post('/shipment/getProvince', [ShipmentService::class, 'getAvailableProvince']);
-Route::post('/shipment/getCity', [ShipmentService::class, 'getAvailableCity']);
-Route::post('/shipment/getDistrict', [ShipmentService::class, 'getAvailableDistrict']);
-Route::post('/shipment/calculateCost', [ShipmentService::class, 'getShippingCost']);
+Route::post('/shipment/getProvince', [ShipmentController::class, 'getAvailableProvince']);
+Route::post('/shipment/getCity', [ShipmentController::class, 'getAvailableCity']);
+Route::post('/shipment/getDistrict', [ShipmentController::class, 'getAvailableDistrict']);
+Route::post('/shipment/calculateCost', [ShipmentController::class, 'getShippingCost']);
